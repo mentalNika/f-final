116469dcab1d22e9285d01b248336ed09238a330
mentalNika <mandrygina798@mail.ru>
2024-05-30 21:10:23 +0300

    first commit

---
+++ app/api/config.js
@@ -0,0 +1,8 @@
+export const BASE_URL = "https://pindie-haiks1.nomoredomainswork.ru/api";
+// export const BASE_URL = "https://rm-pindie-back.nomoredomainswork.ru/api";
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+};
